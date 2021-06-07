1fe9db3aa5d9d8cfff41f828d1804361516205ae
kenykenoei <matheuschavier112004@gmail.com>
2021-06-07 01:39:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let fruits=["banana","laranja","pera","manga","coco"]
+console.log(fruits)
+fruits.unshift("tomate")
+fruits.push("maçã","mamão")
+const numbers=[7,5,6,3,8,9,1,4]
+numbers.sort()
+console.log(numbers)
